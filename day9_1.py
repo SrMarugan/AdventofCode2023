59889debfbf172ce4ab4c8ca984c3ecc3d899eaf
SrMarugan <115635629+SrMarugan@users.noreply.github.com>
2023-12-20 19:50:58 +0100

    Day 9 Part 1

---
+++ day9_1.py
@@ -0,0 +1,17 @@
+def dif_lista(lista):
+    if all(x == 0 for x in lista):
+        return 0
+
+    deltas = [y - x for x, y in zip(lista, lista[1:])]
+    diff = dif_lista(deltas)
+    return lista[-1] + diff
+        
+
+with open(file="adventcalendar\Day9\input.txt") as read_file:
+    lines = read_file.readlines()
+    resultado = 0
+    for line in lines:
+        nums = list(map(int, line.split()))
+        resultado += dif_lista(nums)
+
+print(resultado)
